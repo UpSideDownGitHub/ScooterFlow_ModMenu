5da97f198740989a67ad615ea99eb4cd384c56ad
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2022-06-11 16:13:48 +0100

    Initial Commit
    basic functionality of a mod that is loaded and will render a basic window to the screen.

---
+++ USDCModMenu/USDCModMenu/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Harmony12;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace USDCModMenu
+{
+    // Token: 0x02000003 RID: 3
+    internal static class Main
+	{
+		// Token: 0x06000006 RID: 6 RVA: 0x000027F8 File Offset: 0x000009F8
+		private static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			Main.modId = modEntry.Info.Id;
+			HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+			GameObject gameObject = new GameObject();
+			gameObject.name = "Main Script - UpSideDownCord#1346";
+			gameObject.AddComponent<Mod>();
+			UnityEngine.Object.DontDestroyOnLoad(gameObject);
+			return true;
+		}
+
+		// Token: 0x0400000E RID: 14
+		public static string modId;
+	}
+}
---
+++ USDCModMenu/USDCModMenu/Mod.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace USDCModMenu
+{
+    public class Mod : MonoBehaviour
+	{
+		// Token: 0x06000007 RID: 7 RVA: 0x000028B8 File Offset: 0x00000AB8
+		public void Start()
+		{
+
+		}
+
+		// Token: 0x06000008 RID: 8 RVA: 0x00002918 File Offset: 0x00000B18
+		public void Update()
+		{
+
+		}
+		// Token: 0x0600000B RID: 11 RVA: 0x00002ADC File Offset: 0x00000CDC
+		public void Gui()
+		{
+
+			GUI.Box(new Rect(1515f, 0f, 165f, 220f), "Extras Menu");
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00002DA0 File Offset: 0x00000FA0
+		public void Menu1()
+		{
+
+		}
+
+
+		// Token: 0x06000011 RID: 17 RVA: 0x00004F83 File Offset: 0x00003183
+		public void OnGUI()
+		{
+			this.Gui();
+			this.Menu1();
+		}
+	}
+}
