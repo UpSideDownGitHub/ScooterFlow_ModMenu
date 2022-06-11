cc35d35325ad2553cbae9f5c9e4f98adcad80dac
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2022-06-11 18:58:00 +0100

    Error with patching
    giving me an error on the type even tho the type is correct so not sure what the error is going to try with a non private method to see if that works then if it does we will have to see what happens

---
+++ USDCModMenu/USDCModMenu/Patch1.cs
@@ -0,0 +1,54 @@
+﻿using Harmony12;
+using Rewired;
+
+namespace USDCModMenu
+{
+    [HarmonyPatch(typeof(AnimationTrigger), "Update")]
+	class Patch1
+	{
+		static bool Postfix(AnimationTrigger __instance)
+		{
+			if (__instance.R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[0];
+			}
+			if (__instance.R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[1];
+			}
+			if (__instance.L1R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[2];
+            }
+			if (__instance.L1R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[3];
+			}
+			if (__instance.R2Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[4];
+			}
+			if (__instance.R2L2Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[5];
+			}
+			if (__instance.L1R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[6];
+			}
+			if (__instance.L1R1Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[7];
+			}
+			if (__instance.R2Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[8];
+			}
+			if (__instance.R2L2Trick)
+			{
+				Mod.mod.anim.speed = Mod.mod.AnimationSpeeds[9];
+			}
+			return true;
+		}
+	}
+}
